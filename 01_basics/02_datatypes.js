d62f65a4a3536d6fd0b6492584ba329af2e518ed
Amit Sharma <mail4amit04@gmail.com>
2026-01-20 15:18:40 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS Code as newer version
+
+// code readability should be high
+
+let name = "Amit"
+let age = 21
+let isLoggedIn = "True"
+
+
+
+// nuber => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+
+
+console.log(typeof age);
+console.log(typeof isLoggedIn)
+console.log(typeof null)
