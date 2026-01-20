1548afd9096788781d35344c11b733c095f63ea5
Amit Sharma <mail4amit04@gmail.com>
2026-01-20 17:17:33 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,20 +1,20 @@
-let score = "Amit"
+//let score = "Amit"
 
-console.log(typeof score)
+//console.log(typeof score)
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "Amit"
+//let isLoggedIn = "Amit"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 
 
@@ -23,8 +23,40 @@ console.log(booleanIsLoggedIn);
 // "Amit" => true
 
 
-let someNumber = 57
+//let someNumber = 57
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ****************** Operations ****************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Amit"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(2 + 3 + 4 + "4");
+
+
+// if value started with string all the values are considered in a string
+
+// if value has string in last so starting operation is considered first then string valuye is wriiten
+
+
