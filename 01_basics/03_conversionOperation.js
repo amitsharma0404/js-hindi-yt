977d0d17e4aa23532f0c97d0090ecad8b335a9b2
Amit Sharma <mail4amit04@gmail.com>
2026-01-20 15:56:11 +0530

    conversion operationn

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "Amit"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Amit"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Amit" => true
+
+
+let someNumber = 57
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
