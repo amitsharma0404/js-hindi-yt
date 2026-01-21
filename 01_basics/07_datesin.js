72e43646285838272f5e2a1208ea5ba18c58c6f8
Amit Sharma <mail4amit04@gmail.com>
2026-01-21 18:47:54 +0530

    dates

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,34 @@
+// Dates
+// date is built in object and mainly used to work with dates and times
+let myDate = new Date()
+//console.log(myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+let myCreatedDate = new Date(2026, 12, 21)
+//let myCreatedDate = new Date(2026, 1, 21, 4, 78)
+//let myCreatedDate = new Date("2026-01-11")
+//console.log(myCreatedDate);
+//console.log(myCreatedDate.toDateString());
+//console.log(myCreatedDate.toLocaleString());
+
+
+
+
+
+// timestamp is used in quiz, polls to check how fast and who choose the first
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
