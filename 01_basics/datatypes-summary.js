6f2fa7dee53d7f6f243dd4b40cc2e094af093b02
Amit Sharma <mail4amit04@gmail.com>
2026-01-21 10:57:16 +0530

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, symbol, Bigint
+
+const score = 100
+const isLoggedIn = false
+const outTemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 3736886482828n
+
+
+
+// Reference (Non Primitive)
+
+// array, objects, functions
+
+//const heros = ["shaktimaan", "naagraj", "aoga"];
+//let myObj = {
+        name: "Amit"
+        age : 22,
+     //}
+
+//const myFunction = function(){
+  //  console.log("Hello World");
+    
+//}
+
+console.log(typeof score);
+console.log(typeof outTemp);
+console.log(typeof bigNumber);
+console.log(typeof isLoggedIn);
+
+
+
+
+
