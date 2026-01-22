a787d859320521c29bd12fd1cd86de60b3c8325d
Amit Sharma <mail4amit04@gmail.com>
2026-01-22 13:06:42 +0530

    array 02

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,38 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+//marvel_heros.push(dc_heros)
+// push add elements on existing array
+
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][1]);
+
+//const allHeros = marvel_heros.concat(dc_heros)
+
+//concat add two array and return in a new array
+//console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+// ... is a spread operator which spread the elements of two or more array in a single array
+
+//console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+
+//console.log(another_array);
+//console.log(real_another_array);
+
+console.log(Array.isArray("Amit"))
+console.log(Array.from("Amit"));
+console.log(Array.from({name: "Amit"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+
+
