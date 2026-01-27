282042e21b62f2abd6e2cccc82f84a884f1be19c
Amit Sharma <mail4amit04@gmail.com>
2026-01-27 17:31:02 +0530

    four

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+// const myobject = {
+//     js : 'javascript',
+//     cpp : 'C++',
+//     rb : 'ruby',
+//     swift : 'swift by apple'
+// }
+
+// for (const key in myobject) {
+    
+//     console.log(myobject[key]);
+    
+    
+// }
+
+// for (const key in myobject) {
+    
+//     console.log(`${key} shorcut is for ${myobject[key]}`);
+    
+    
+// }
+
+// const programming = ['js', 'rb', 'py', 'java', 'cpp']
+
+// for (const key in programming) {
+//     console.log(programming[key]);
+    
+    
+    
+// }
