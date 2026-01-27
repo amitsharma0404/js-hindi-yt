129e8e46e4d46fc98ea651a8124f86fa44f558c5
Amit Sharma <mail4amit04@gmail.com>
2026-01-27 16:51:01 +0530

    three

---
+++ 05_iterations/three.js
@@ -0,0 +1,47 @@
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+    
+// }
+
+// const greetings = "Hello world!"
+
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+    
+// }
+
+
+// Maps
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('PAK', "Pakistan")
+
+// //console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+    
+// }
+// for (const [key, value] of map) {
+//     console.log(key);
+    
+// }
+// for (const [key, value] of map) {
+//     console.log(key, ":-", value);
+    
+// }
+
+const myobject = {
+    'game1' : 'Pubg',
+    'game2' : 'Spiderman'
+}
+
+for (const [key, value] of myobject) {
+    console.log(key, ':', value);
+    
+}
